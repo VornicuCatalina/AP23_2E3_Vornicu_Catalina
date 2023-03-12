d40b17169bd6a9f10717d8e72c464f13a310ec13
VornicuCatalina <100404375+VornicuCatalina@users.noreply.github.com>
2023-03-13 00:06:31 +0200

    Add files via upload

--- LAB3/Homework/Network.java
+++ LAB3/Homework/Network.java
@@ -7,25 +7,6 @@ import java.util.List;
 
 public class Network {
     private List<Node> nodes=new ArrayList<>();
-    //for last task
-    private int timer;
-    private int NoNodes;
-    private LinkedList<Integer> adj =new LinkedList<Integer>();
-    private boolean[] visited;
-    private int[] tin;
-    private int[] low;
-    public void findCutPoints(){
-        /*
-        timer = 0;
-    visited.assign(n, false);
-    tin.assign(n, -1);
-    low.assign(n, -1);
-    for (int i = 0; i < n; ++i) {
-        if (!visited[i])
-            dfs (i);
-    }
-         */
-    }
     public void addNode(Node n){
         nodes.add(n);
     }
