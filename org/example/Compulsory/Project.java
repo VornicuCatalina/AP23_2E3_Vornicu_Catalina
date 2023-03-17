af364f7b661c7b84309731011086dbe9f589f57b
VornicuCatalina <100404375+VornicuCatalina@users.noreply.github.com>
2023-03-17 11:41:44 +0200

    Add files via upload

---
+++ org/example/Compulsory/Project.java
@@ -0,0 +1,16 @@
+package org.example.Compulsory;
+
+public class Project implements Comparable<Project> {
+    private String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public int compareTo(Project other) {
+        return this.name.compareTo(other.name);
+    }
+}
